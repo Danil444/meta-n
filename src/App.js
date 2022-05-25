4b026e684c86d617f189395fe4e2a92e636de134
danil444 <danil.44443@gmail.com>
2022-05-26 00:51:52 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,167 @@
+import React from 'react';
+import css from './style.css'
+
+function App() {
+  return (
+    <div class="container">
+      <style>{'body { background-color: #071711;}'}</style>
+      
+      <nav class="top-menu">
+        <a class="navbar-logo" href=""><img src="logo.png"/></a>
+        <ul class="menu-main">
+          <li><a href="#features">Features</a></li>
+          <li><a href="#">RoadMap</a></li>
+          <li><a href="#business">Business model</a></li>
+          <li><a href="#">Partners</a></li> 
+        </ul>
+      </nav>
+      <div id="rectangle"></div>
+      <h1 class="text1">VALUE CREATION<br/>
+      THOUGH <span class="color" >META-NFT</span><br/>
+      ECOSYSTEM</h1>
+      <h4 class="text2">Be a part of a democratized metaverse economy<br/>
+      Unlock financial opportunities with one-of-a-kind marketplace<br/>
+      model.</h4>
+      <img class="img1" href="" src="images/img1.png"/>
+      <a class="a1"  href="https://www.google.ru/">JOIN DISCORD</a>
+      <img class="img2" href="" src="images/img2.png"/>
+      <img class="img3" href="" src="images/img3.png"/>
+      <img class="img4" href="" src="images/img4.png"/>
+      <img class="img5" href="" src="images/img5.png"/>
+      <img class="img6" href="" src="images/img6.png"/><p/>
+      <img class="img7" href="" src="images/img7.png"/>
+      <img class="img8" href="" src="images/Arrow.png"/>
+      <h1 class="text3" id='features'>Features</h1>
+      <div class="iconblock1">
+        <div class="icon">
+          <img src="images/icon1.png"/>          
+        </div>
+      <h3>COMMUNITY-<br/>DRIVEN</h3>
+      <p class="p">INSTEAD JF THE TYPICAL<br/>
+      BUYER-SELLER DRIVEN<br/>
+      MARKET, META-N<br/>
+      ALLOWS ALL STAKEHOLDERS<br/>
+      TO PARTICIPATE<br/>
+      THE VALUE<br/>
+      CHAIN IN THE<br/>
+      METAVERSE SPACE</p>
+      </div>
+      <div class="iconblock2">
+        <div class="icon">
+          <img src="images/icon2.png"/>
+        </div>
+        <h3>FAIR PRICE<br/>
+        DISCOVERY</h3>
+        <p class="p1">A FAIR AND TRANSPARENT<br/>
+        PRICING FOR NFTs THAT IS<br/>
+        DRIVEN BY THE COMMUNITY<br/> 
+        PRESENT IN THE<br/>
+        METAVERSE SPACE</p>
+      </div>
+      <div class="iconblock3">
+        <div class="icon">
+          <img src="images/icon3.png"/>
+  
+        </div>
+        <h3>LOW<br/>
+        BARRIERS</h3>
+        <p class="p1">BUILT ON LAYER2 SOLITION<br/>
+        WHICH ENABLES LOW<br/>
+        GAS FEESAND<br/>
+        ISENVIRONMENTALLY<br/>
+        SUSTAINABLE</p>
+      </div>
+      <div class="iconblock4">
+        <div class="icon">
+          <img src="images/icon4.png"/>
+  
+        </div>
+        <h3>ENHANCING<br/>
+        METAVERSE<br/>
+        EXPERIENCE</h3>
+        <p class="p2">CREARORS AND BIDDERS<br/>
+        COME FACE TO FACE<br/>
+        IN THE<br/>
+        SURREAL WORLD FOR THE<br/>
+        VALUE CREATION</p>
+      </div>
+      <div class="iconblock5">
+        <div class="icon">
+          <img src="images/icon5.png"/>
+  
+        </div>
+        <h3>REVERSE<br/>
+        BIDDING</h3>
+        <p class="p1">METAVERSE PARTICIPANTS<br/>
+        WOULD PUT THE REQUEST<br/>
+        FOR THE UTILITIES WHICH<br/>
+        WILL BE DRIVEN BY<br/>
+        THE NFTs</p>
+      </div>
+      <h1 class="text4"><span class="color" >META-N</span> VALUE PROPOSITION</h1>
+      <div class="iconblock6">
+        <div class="icon">
+          <img src="images/Bidder.png"/>          
+        </div>
+      <h3>BIDDER</h3>
+      <p class="ntext">Fair price discovery of the ART determined<br/>
+      collectively by the community. The entire<br/>
+      process is transparent in all aspects<br/></p>
+      </div>
+      <div class="iconblock6">
+        <div class="icon1">
+          <img src="images/IPFS.png"/>          
+        </div>
+      <h3 class="h31">IPFS</h3>
+      <p class="ntext1">Storing digital collectables on-chain<br/>
+      using IPFS (or the InterPlanetary File System)</p>
+      </div>
+      <div class="iconblock6">
+        <div class="icon">
+          <img src="images/NFTrentals.png"/>          
+        </div>
+      <h3>NFT RENTALS</h3>
+      <p class="ntext">Rent digital collectibles in the secondary<br/>
+      marketplace to earn passive income</p>
+      </div>
+      <div class="iconblock6">
+        <div class="icon1">
+          <img src="images/Bragging.png"/>          
+        </div>
+      <h3 class="h31">BRAGGING RIGHTS</h3>
+      <p class="ntext1">Receives the bragging rights for ownership<br/>
+      of NFTs</p>
+      </div>
+      <div class="iconblock6">
+        <div class="icon">
+          <img src="images/Borrowing.png"/>          
+        </div>
+      <h3>BORROWING ON NWTs</h3>
+      <p class="ntext">Use NFTs as collateral along with crypto<br/>
+      assets to borrow loans</p>
+      </div>
+      <h1 class="text5" id="business"><span class="color" >ALL-INCLUSIVE</span> BUSINESS MODEL</h1>
+      <div class="textblock">
+        <h2>Meta-N business model is based on driving value for all stakeholdersby creating<br/> 
+        an incremental NFT-metaverse ecosystem</h2>
+      </div>
+      <div class="business1">
+        <div class="icon">
+          <img src="images/Creator.png"/>          
+        </div>
+      <h3>Creator lists ART<br/>
+      on Meta-N marketplace<br/>
+      and defines a bidding period</h3>
+      </div>
+      <div class="business2">
+        <div class="icon">
+          <img src="images/Bidders.png"/>          
+        </div>
+      <h3>Bidders can bid on the NFT<br/>
+      during the bidding period<br/><br/></h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
